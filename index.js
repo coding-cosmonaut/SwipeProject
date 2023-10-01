a025c559b8f428c270fd4e64df5a01aeffc17fe8
Alex Morgun <aamorgun2003@gmail.com>
2023-10-01 00:00:50 -0600

    third commit completed mobile nav; added animation for mobile nav

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let hamBar2 = document.querySelector('#line2');
 let hamBar3 = document.querySelector('#line3');
 
 hamButn.addEventListener('click', () => {
+    if (!hamButn.classList.contains("hamburger-button-clicked")) {
+        hamButn.classList.add("hamburger-button-clicked");
+    } else {
+        hamButn.classList.remove("hamburger-button-clicked");
+    }
     mobileNav.classList.toggle("mobile-toggle");
     hamBar1.classList.toggle('hamburger-top-flip');
     hamBar2.classList.toggle('hamburger-middle');
